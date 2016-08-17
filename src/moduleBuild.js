05ed6a8dad8d4ffbafaaf620d2a42f1a8cdc844a
Jonny Buchanan <jonathan.buchanan@gmail.com>
2016-08-17 11:21:19 +1000

    Make not transpiling ES6 modules the default and opt-in for the npm ES5 build

--- src/moduleBuild.js
+++ src/moduleBuild.js
@@ -123,6 +123,8 @@ export default function moduleBuild(args, buildConfig = {}, cb) {
       'ES5',
       {copyFiles, outDir: path.resolve('lib'), src},
       merge(buildConfig.babel, buildConfig.babelDev || {}, {
+        // Transpile modules to CommonJS for ES5 users
+        modules: 'commonjs',
         // Don't force ES5 users of the ES5 build to eat a .require
         plugins: [require.resolve('babel-plugin-add-module-exports')],
       }),
@@ -137,10 +139,7 @@ export default function moduleBuild(args, buildConfig = {}, cb) {
     tasks.push((cb) => runBabel(
       'ES6 modules',
       {copyFiles, outDir: path.resolve('es'), src},
-      merge(buildConfig.babel, buildConfig.babelDev || {}, {
-        // Don't transpile modules, for use by ES6 module bundlers
-        modules: false,
-      }),
+      merge(buildConfig.babel, buildConfig.babelDev || {}),
       userConfig.babel,
       cb
     ))
